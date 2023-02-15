f08872084cbd0440511550712f0ca4a701d81f6d
jupemara <jumpei.arashi@arashike.com>
2023-02-16 04:27:50 +0900

    terraform fmt

--- shared-vpc/dev/locals.tf
+++ shared-vpc/dev/locals.tf
@@ -2,8 +2,8 @@ locals {
   env = "dev"
   subnets = {
     "${local.env}-${local.service_project_id_suffix}" : {
-      primary_cidr = "192.168.0.0/24",
-      region       = "asia-northeast1",
+      primary_cidr = "192.168.0.0/24"
+      region       = "asia-northeast1"
     }
   }
   vpc_serverless_access_connector = {
--- shared-vpc/prd/locals.tf
+++ shared-vpc/prd/locals.tf
@@ -3,7 +3,8 @@ locals {
   subnets = {
     "${local.env}-${var.service_project_id_suffix}" : {
       primary_cidr = "10.0.0.0/20"
-    region = "asia-northeast1" }
+      region       = "asia-northeast1"
+    }
   }
   vpc_serverless_access_connector {
     _cidr  = "10.255.10.0/28"
